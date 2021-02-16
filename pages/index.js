faabadcdce73b46d032207d52bba53721e2e2dce
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-02-16 15:22:09 +0530

    add conditional styling

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from 'next/head'
 
 export default function Home() {
+  const x = 1
   return (
     <div>
       <Head>
         <title>Welcome To First Next.JS</title>
       </Head>
-        <h1>Welcome to Next.Js</h1>
+        <h1>Welcome to <span className='title'>Next.Js</span></h1>
+        <style jsx>
+          {`
+            .title{
+              color: ${x>3 ? 'red' : 'blue'};
+            }
+          `} 
+        </style>
     </div>
   )
 }
